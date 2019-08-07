a634a068d7a711d9a5b7f054c6e8ce139e74fa06
Zachary Amerman <leetong.amerman@gmail.com>
2019-08-07 13:18:52 +0000

    Changed file handlers to create json file in home directory

--- src/main/python/com/revature/setup.py
+++ src/main/python/com/revature/setup.py
@@ -2,10 +2,12 @@ import os
 
 
 def main():
+    home = os.getenv('HOME')
     os.system('sudo mkdir /var/log/bank')
+    os.system('sudo mkdir ' + home + '/bank')
     os.system('sudo touch /var/log/bank/all.log')
     os.system('sudo touch /var/log/bank/error.log')
-    os.system('sudo touch /var/log/bank/vault.json')
+    os.system('sudo touch ' + home + '/bank/vault.json')
 
 
 if __name__ == '__main__':
--- src/main/python/com/revature/uninstall.py
+++ src/main/python/com/revature/uninstall.py
@@ -2,7 +2,9 @@ import os
 
 
 def main():
+    home = os.getenv('HOME')
     os.system('sudo rm -r /var/log/bank')
+    os.system('sudo rm -r ' + home + '/bank')
 
 
 if __name__ == '__main__':
