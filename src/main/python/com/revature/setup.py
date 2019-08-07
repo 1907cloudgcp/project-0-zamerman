a77d9265cf8d034f633737cd9758106f9ccd55f9
Zachary Amerman <leetong.amerman@gmail.com>
2019-08-07 03:08:50 +0000

    Coded file creation in setup script

--- src/main/python/com/revature/setup.py
+++ src/main/python/com/revature/setup.py
@@ -2,7 +2,10 @@ import os
 
 
 def main():
-    pass
+    os.system('sudo mkdir /var/log/bank')
+    os.system('sudo touch /var/log/bank/all.log')
+    os.system('sudo touch /var/log/bank/error.log')
+    os.system('sudo touch /var/log/bank/vault.json')
 
 
 if __name__ == '__main__':
