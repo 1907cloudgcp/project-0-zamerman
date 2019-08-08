bc15cf90176d44978a6e71477a32b27ca325c58b
Zachary Amerman <leetong.amerman@gmail.com>
2019-08-07 21:02:23 -0400

    Changed method names

--- bankapp/IO/Client.py
+++ bankapp/IO/Client.py
@@ -3,6 +3,7 @@ import logging
 import logging.config
 import yaml
 
+
 class Client():
     """
     Data class which contains fields and methods for Client's username,
@@ -10,40 +11,79 @@ class Client():
     """
 
     def __init__(self, username, password, balance, transactions):
+        # Set Client fields
         self.__username = username
         self.__password = password
         self.__balance = balance
         self.__transactions = transactions
 
-        config_path = '/vagrant/project-0-zamerman/src/main/resources/logging.yaml'
+        # Load logging configuration yaml file
+        config_path = '''/vagrant/project-0-zamerman/bankapp/resources/logging.yaml'''
         config = ''
         with open(config_path, 'r') as f:
             config = yaml.safe_load(f.read())
         logging.config.dictConfig(config)
 
+        # Create Client logger
         self.__logger = logging.getLogger(__name__)
-        self.__logger.info('Client created with name: ' + __name__)
 
-    def getUsername(self):
+        # Send info log upon Client creation
+        self.__logger.info('Client created; ' +
+                           'username: ' + username +
+                           '; password: ' + password)
+
+    def get_username(self):
+        """
+        Get client's username
+        """
+        self.__logger.info('Client username requested')
         return self.__username
 
-    def setUsername(self, username):
+    def set_username(self, username):
+        """
+        Set client's username
+        """
         self.__username = username
+        self.__logger.info('Client username changed to ' + username)
 
-    def getPassword(self):
+    def get_password(self):
+        """
+        Get client's password
+        """
+        self.__logger.info('Client password requested')
         return self.__password
 
-    def setPassword(self, password):
+    def set_password(self, password):
+        """
+        Set client's password
+        """
         self.__password = password
+        self.__logger.info('Client password changed to ' + password)
 
-    def getBalance(self):
+    def get_balance(self):
+        """
+        Get client's balance
+        """
+        self.__logger.info('Client balance requested')
         return self.__balance
 
-    def setBalance(self, balance):
+    def set_balance(self, balance):
+        """
+        Set client's balance
+        """
         self.__balance = balance
+        self.__logger.info('Client balance changed to ' + balance)
 
-    def getTransactions(self):
+    def get_transactions(self):
+        """
+        Get client's transactions
+        """
+        self.__logger.info('Client transactions requested')
         return self.__transactions
 
-    def setTransactions(self, transactions):
+    def set_transactions(self, transactions):
+        """
+        Set client's transactions
+        """
         self.__transactions = transactions
+        self.__logger.info('Client transactions changed')
