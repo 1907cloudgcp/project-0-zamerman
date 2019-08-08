8e0044fa4104606fcebf88c3de16fd6b37cba3bb
Zachary Amerman <leetong.amerman@gmail.com>
2019-08-08 13:24:04 -0400

    Coded IO files

--- src/IO/Client.py
+++ src/IO/Client.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
-import logging
-import logging.config
-import yaml
 
 
 class Client():
     """
     Data class which contains fields and methods for Client's username,
     password, balance, and transactions.
+
+    username : unique identifier for Client objects : what is compared
     """
 
     def __init__(self, username="", password="", balance=0, transactions=[]):
-        # Set Client fields
+        """
+        Sets client fields
+        """
+
+        # set Client fields
         self.username = username
         self.password = password
         self.balance = balance
@@ -21,7 +24,6 @@ class Client():
         """
         Get client's username
         """
-        __get_logger().info('Client username requested')
         return self.username
 
     def set_username(self, username):
@@ -29,13 +31,11 @@ class Client():
         Set client's username
         """
         self.username = username
-        __get_logger().info('Client username changed to ' + username)
 
     def get_password(self):
         """
         Get client's password
         """
-        __get_logger().info('Client password requested')
         return self.password
 
     def set_password(self, password):
@@ -43,13 +43,11 @@ class Client():
         Set client's password
         """
         self.password = password
-        __get_logger().info('Client password changed to ' + password)
 
     def get_balance(self):
         """
         Get client's balance
         """
-        __get_logger().info('Client balance requested')
         return self.balance
 
     def set_balance(self, balance):
@@ -57,13 +55,11 @@ class Client():
         Set client's balance
         """
         self.balance = balance
-        __get_logger().info('Client balance changed to ' + balance)
 
     def get_transactions(self):
         """
         Get client's transactions
         """
-        __get_logger().info('Client transactions requested')
         return self.transactions
 
     def set_transactions(self, transactions):
@@ -71,16 +67,10 @@ class Client():
         Set client's transactions
         """
         self.transactions = transactions
-        __get_logger().info('Client transactions changed')
 
-    @staticmethod
-    def __get_logger():
-        # Load logging configuration yaml file
-        config_path = '''/vagrant/project-0-zamerman/bankapp/resources/logging.yaml'''
-        config = ''
-        with open(config_path, 'r') as f:
-            config = yaml.safe_load(f.read())
-        logging.config.dictConfig(config)
-
-        # Create Client logger
-        return logging.getLogger(__name__)
+    def __eq__(self, other_client):
+        """
+        Compares client usernames
+        """
+        username_check = self.username == other_client.get_username()
+        return username_check
--- src/IO/ClientDAOJSON.py
+++ src/IO/ClientDAOJSON.py
@@ -1,34 +1,99 @@
+from .ClientDAO import ClientDAO
+from .Client import Client
+import json
 import sys
 import os
-import json
 
-sys.path.insert(0, '/'.join(os.getcwd().split('/')[:-1]))
-
-from IO.ClientDAO import ClientDAO
-from IO.Client import Client
 
 class ClientDAOJSON(ClientDAO):
-    def __init__(self, json_file):
-        self.__json_file = json_file
+    """
+    JSON access object
+    """
+
+    def __init__(self, vault_file):
+        """
+        vault_file : path to json vault file
+        """
+        self.vault_file = vault_file
 
     def get_clients(self):
-        with open(self.__json_file, 'r') as f:
+        """
+        returns a list of clients in the vault
+        """
+        with open(self.vault_file, 'r') as f:
             client_dicts = json.load(f)
-        try:
-            clients = [Client(dict['username'], dict['password'], dict['balance'], dict['transactions']) for dict in client_dicts]
-        except ValueError:
-            clients = []
+        clients = []
+        for dict in client_dicts:
+            new_client = Client()
+            new_client.__dict__ = dict
+            clients.append(new_client)
         return clients
 
     def add_client(self, new_client):
+        """
+        adds a client to the vault unless that client is already in the vault
+
+        clients are uniquely identified by username so even if two clients with
+        the same username have different passwords they are still considered
+        the same
+        """
+
+        # get a list of clients
         clients = self.get_clients()
-        clients.append(new_client)
+
+        # check if the client is already in the vault before adding
+        if new_client not in clients:
+            clients.append(new_client)
+
+        # write the updated vault list to the vault
         client_dicts = [client.__dict__ for client in clients]
-        with open(self.__json_file, 'w') as f:
+        with open(self.vault_file, 'w') as f:
             json.dump(client_dicts, f)
 
-    def update_client(self):
-        pass
+    def update_client(self, client, new_client):
+        """
+        changes a client in the vault unless a client with the same username as
+        new_client already exists
+        
+        always deletes old client
 
-    def delete_client(self):
-        pass
+        clients are uniquely identified by username so even if two clients with
+        the same username have different passwords they are still considered
+        the same
+        """
+
+        # get a list of clients
+        clients = self.get_clients()
+
+        # remove old client
+        clients.remove(client)
+
+        # check if the updated client is already in the vault before adding
+        if new_client not in clients:
+            clients.append(new_client)
+
+        # write the updated vault list to the vault
+        client_dicts = [client.__dict__ for client in clients]
+        with open(self.vault_file, 'w') as f:
+            json.dump(client_dicts, f)
+
+
+    def delete_client(self, client):
+        """
+        deletes client from vault
+
+        clients are uniquely identified by username so even if two clients with
+        the same username have different passwords they are still considered
+        the same
+        """
+
+        # get a list of clients
+        clients = self.get_clients()
+
+        # remove client
+        clients.remove(client)
+
+        # write the updated vault list to the vault
+        client_dicts = [client.__dict__ for client in clients]
+        with open(self.vault_file, 'w') as f:
+            json.dump(client_dicts, f)
