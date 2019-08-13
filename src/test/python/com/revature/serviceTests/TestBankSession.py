0532d6f0fe7969b1da17872c1ad0df62bc7ef70a
Zachary Amerman <leetong.amerman@gmail.com>
2019-08-13 15:54:06 -0400

    Wrote login test cases

--- src/test/python/com/revature/serviceTests/TestBankSession.py
+++ src/test/python/com/revature/serviceTests/TestBankSession.py
@@ -1,7 +1,7 @@
 # Import standard python modules
-import unittest
 import os
 import sys
+import unittest
 
 # Get path to main program files
 test_path = os.path.abspath(__file__).split('/')
@@ -16,19 +16,22 @@ sys.path.insert(0, main_path)
 from IO.Client import Client
 from IO.ClientDAOJSON import ClientDAOJSON
 from service.BankSession import BankSession
-from error.Error import ClientRequiredError, ClientSetupError
+from error.Error import ClientSetupError, LoginError
 
 class TestBankSession(unittest.TestCase):
     def setUp(self):
         # Code to assemble and put together path to vault file relative to file
-    	file_path = os.path.abspath(__file__)
-    	vault_path = file_path.split('/')[:-7]
-    	vault_path.append('data/vault.json')
-    	vault_path = '/'.join(vault_path)
-    	self.client_dao = ClientDAOJSON(vault_path)
+        file_path = os.path.abspath(__file__)
+        vault_path = file_path.split('/')[:-7]
+        vault_path.append('data/vault.json')
+        vault_path = '/'.join(vault_path)
+
+        # Instantiate a ClientDAO
+        self.client_dao = ClientDAOJSON(vault_path)
+
+        # Instantiate a BankSession
         self.bank_session = BankSession(self.client_dao)
 
-        
     def tearDown(self):
         # Clear vault
         self.client_dao.clear_clients()
@@ -38,13 +41,27 @@ class TestBankSession(unittest.TestCase):
         self.assertEqual(self.bank_session.client_dao, self.client_dao)
 
     def test_current_session(self):
-        self.assertEqual(self.bank_session.current_session(), Client())
+        self.assertEqual(self.bank_session.current_session(), '')
 
     def test_register(self):
         self.bank_session.register(Client())
         self.assertEqual(self.bank_session.client, Client())
         self.assertEqual(self.client_dao.get_clients(), [Client()])
 
+    def test_register_fail(self):
+        self.bank_session.register(Client())
+        with self.assertRaises(ClientSetupError):
+            self.bank_session.register(Client())
+
+    def test_login(self):
+        self.client_dao.add_client(Client('username', 'password'))
+        self.bank_session.login(Client('username', 'password'))
+        self.assertEqual(self.bank_session.current_session(), 'username')
+
+    def test_login_fail(self):
+        self.client_dao.add_client(Client('username', 'password'))
+        with self.assertRaises(LoginError):
+            self.bank_session.login(Client('username1', 'password1'))
 
 if __name__ == '__main__':
     unittest.main()
